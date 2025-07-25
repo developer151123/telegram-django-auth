438164be1449cd209cc78957c5013eb67861fd1a
developer151123 <developer151123@gmail.com>
2025-07-25 19:13:39 +0300

    Qr code works

--- telegram_django_auth/urls.py
+++ telegram_django_auth/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from telegram_django_auth.views import qr_login_view, change_language, telegram_view, phone_login_view
+from telegram_django_auth.views import qr_login_view, change_language, telegram_view, phone_login_view, qr_wait_login
 
 urlpatterns = [
     path('', telegram_view, name='telegram'),
     path('qr/', qr_login_view, name='login-qr'),
+    path('qr-wait-login/', qr_wait_login, name='login-qr-wait'),
     path('admin/', admin.site.urls),
     path('phone/', phone_login_view, name='login-by-phone'),
     path('change-language/', change_language, name='change_language'),
--- telegram_django_auth/views.py
+++ telegram_django_auth/views.py
@@ -1,6 +1,10 @@
 import io
+from urllib.parse import urlparse, parse_qs
+
+from django.http import JsonResponse, request
 from django.shortcuts import render, redirect
 from django.utils.translation import activate
+from telethon.tl.custom import QRLogin
 
 from telegram_django_auth import settings
 from telegram_django_auth.countries import countries
@@ -13,40 +17,65 @@ from asgiref.sync import async_to_sync
 def telegram_view(request):
     return set_language_and_load(request)
 
-
 # Replace these with your app's Telegram API credentials
 API_ID = 20851336
 API_HASH = "9965c9d43d7dae53c4d7d42cd22f21ac"
 
 # Use a session with Telethon (use StringSession for flexibility)
 SESSION = StringSession()
+client = TelegramClient("SessionName", API_ID, API_HASH)
+
+loginSessions = {}
 
 # Global variable for storing the QR login session (can also store in Django sessions)
-qr_login = None
-
 @async_to_sync
-async def generate_qr_url() -> str:
+async def generate_qr_url(request) -> str:
 
-    global qr_login
-    client = TelegramClient(SESSION, API_ID, API_HASH)
-    await client.connect()
+    token = request.GET.get('token')
+    if token:
+        qrlo = loginSessions[token]
+        return qrlo.url
+
+    if not client.is_connected():
+        await client.connect()
 
     # Ensure the client is not already authorized
     if not await client.is_user_authorized():
         # Get the QR Login object
         print("Client is not authorized. Generating QR login session...")
-        qr_login = await client.qr_login()  # Generate the QR login object
-        return qr_login.url
+        qrlo = await client.qr_login()
+        parsed_url = urlparse(qrlo.url)
+        token = parse_qs(parsed_url.query)['token'][0]
+        # Generate the QR login object
+        loginSessions[token] = qrlo
+        return qrlo.url
     else:
         print("Client is already authorized.")
         return ""
 
-
 def qr_login_view(request):
     lang_code = request.session[settings.LANGUAGE_COOKIE_NAME]
     activate(lang_code)
-    qr_url =  generate_qr_url()
-    return render(request, 'login-qr.html', {'lang': request.session[settings.LANGUAGE_COOKIE_NAME], 'url': qr_url})
+    qr_url =  generate_qr_url(request)
+    parsed_url = urlparse(qr_url)
+    token = parse_qs(parsed_url.query)['token'][0]
+    return render(request, 'login-qr.html',{'lang': request.session[settings.LANGUAGE_COOKIE_NAME], 'url': qr_url, 'token': token})
+
+
+async def qr_wait_login(request):
+    # Simulate data to be fetched
+    data = {
+        "timeout": False,
+        "result": False
+    }
+
+    # Important! You need to wait for the login to complete!
+    try:
+        qrlo = loginSessions[request.GET.get('token')]
+        data.result = await qrlo.wait(10)
+    except TimeoutError:
+        data["timeout"] = True
+    return JsonResponse(data)
 
 def phone_login_view(request):
     lang_code = request.session[settings.LANGUAGE_COOKIE_NAME]
@@ -58,8 +87,10 @@ def set_language_and_load(request):
     lang_code = 'en'  # Get the language code from the query param
     request.session[settings.LANGUAGE_COOKIE_NAME] = lang_code
     activate(lang_code)  # Activate the selected language
-    qr_url = generate_qr_url()
-    return render(request, 'login-qr.html', {'lang': request.session[settings.LANGUAGE_COOKIE_NAME], 'url': qr_url})
+    qr_url = generate_qr_url(request)
+    parsed_url = urlparse(qr_url)
+    token = parse_qs(parsed_url.query)['token'][0]
+    return render(request, 'login-qr.html', {'lang': request.session[settings.LANGUAGE_COOKIE_NAME], 'url': qr_url, 'token': token})
 
 def change_language(request):
     """Activate the selected language from the button click."""
